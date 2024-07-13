0a259eb784194edee41ec31a652b0670837980ad
Allen Douglas <allenissabigboi@gmail.com>
2024-07-13 13:25:20 +0100

    Protoype and Array Manipiulation

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
 // Advanced JS - Prototyping
-function Person(fname, lname) {
-    this.firstName = fname;
-    this.lastName = lname;
-}
+// function Person(fname, lname) {
+//     this.firstName = fname;
+//     this.lastName = lname;
+// }
 
 
 
-Person.prototype.returnFullName = function () {
-    console.log(`${this.firstName} ${this.lastName}`)
-}
+// Person.prototype.returnFullName = function () {
+//     console.log(`${this.firstName} ${this.lastName}`)
+// }
 
-const newPerson = new Person("Allen", "Douglas")
+// const newPerson = new Person("Allen", "Douglas")
 
-newPerson.returnFullName()
+// newPerson.returnFullName()
+
+
+
+// Advanced JS - Array Manipulation
+
+const gender = ["male", "female"]
+
+const mappedGender = gender.map(element =>  ({value: element}))
+console.log(mappedGender)
+
+const people = [
+    {
+        name: "Allen",
+        age: "22",
+        gender: gender[0],
+        isHappy: false 
+
+    }, 
+    {
+        name: "Aaron",
+        age: "20",
+        gender: gender[0],
+        isHappy: true   
+    },
+    {
+        name: "Irene",
+        age: "18",
+        gender: gender[1],
+        isHappy: true   
+    }
+]
+
+const happyPeople = people.filter(element => {
+    return element.gender == 'male' 
+})
+
+console.log(happyPeople)
